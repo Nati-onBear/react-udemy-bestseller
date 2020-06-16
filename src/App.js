71b32c4b7784e6a412315d886c4b11f9850fc543
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-06-17 02:39:44 +0700

    32. JSX Restrictions
    Only one root element!
    
    No class for HTML-ish code here!

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //     </header>
-  //     <h1>Hello, I'm learning React again!</h1>
-  //   </div>
-  // );
-  const firstChild = React.createElement('header', {className: 'App-header'}, 
-    React.createElement('img', {src:logo, className:'App-logo', alt:'logo'}))
-  const secondChild = React.createElement('h1', null, 'Hello, I\'m learning React again!')
-  return React.createElement('div', {className:'App'}, firstChild, secondChild )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h1>Hello, I'm learning React again!</h1>
+      <p>Only one root element!</p>
+      <p>No class for HTML-ish code here!</p>
+    </div>
+  );
+  // const firstChild = React.createElement('header', {className: 'App-header'}, 
+  //   React.createElement('img', {src:logo, className:'App-logo', alt:'logo'}))
+  // const secondChild = React.createElement('h1', null, 'Hello, I\'m learning React again!')
+  // return React.createElement('div', {className:'App'}, firstChild, secondChild )
 }
 
 export default App;
