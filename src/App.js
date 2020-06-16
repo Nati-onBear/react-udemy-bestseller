cd53bd0b6f44bbf84fef9e8f06700db9a4235a02
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-06-17 02:35:26 +0700

    31. Understanding JSX
    Not HTML, but JS code being compiled through React.createElement()

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <h1>Hello, I'm learning React again!</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //     </header>
+  //     <h1>Hello, I'm learning React again!</h1>
+  //   </div>
+  // );
+  const firstChild = React.createElement('header', {className: 'App-header'}, 
+    React.createElement('img', {src:logo, className:'App-logo', alt:'logo'}))
+  const secondChild = React.createElement('h1', null, 'Hello, I\'m learning React again!')
+  return React.createElement('div', {className:'App'}, firstChild, secondChild )
 }
 
 export default App;
