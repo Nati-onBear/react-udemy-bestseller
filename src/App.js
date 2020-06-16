dca177ca231c9eabb329d25bfc206b7ec0d9e677
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-06-17 04:05:48 +0700

    45. Using the useState() Hook for State Manipulation
    Wow

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './components/Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Bear', age: 22, hobby: null },
       { name: 'Hung', age: 18, hobby: 'skating' },
       { name: 'Thuy', age: 20, hobby: 'ignoring Hung' },
     ]
-  }
+  })
 
-  switchNameHandler = () => {
-    console.log('Clicked')
-    this.setState({ 
+  const switchNameHandler = () => {
+    setPersonsState({ 
       persons: [
         { name: 'Barry Allen', age: 22, hobby: null },
         { name: 'Hung', age: 18, hobby: 'skating' },
@@ -23,24 +22,19 @@ class App extends Component {
     })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <h1>Hello, I'm learning React again!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name!</button>
-        {this.state.persons.map(person => 
-          <Person name={person.name} age={person.age} hobby={person.hobby}/>
-        )}
-      </div>
-    );
-    // const firstChild = React.createElement('header', {className: 'App-header'}, 
-    //   React.createElement('img', {src:logo, className:'App-logo', alt:'logo'}))
-    // const secondChild = React.createElement('h1', null, 'Hello, I\'m learning React again!')
-    // return React.createElement('div', {className:'App'}, firstChild, secondChild )
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h1>Hello, I'm learning React again!</h1>
+      <button onClick={switchNameHandler}>Switch Name!</button>
+      {personsState.persons.map(person => 
+        <Person name={person.name} age={person.age} hobby={person.hobby}/>
+      )}
+    </div>
+  );
 }
 
 export default App;
+
