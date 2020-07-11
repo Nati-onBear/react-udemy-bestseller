51c15783e2a47629fd5ff2e345d4fe57169c513b
Bear <wokhug99@gmail.com>
2020-07-11 18:53:57 +0700

    98. Optimizing Functional Components with React.memo()

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,39 +1,31 @@
-import React, { Component } from 'react'
+import React, { memo } from 'react'
 import Radium from "radium";
 import './Person.css'
 
-class Person extends Component {
-  style = {
-    '@media (min-width: 500px)': {
+
+
+const person = (props) => {
+  const { name, age, hobby, click, changed } = props
+  const style = {
+    '@media (minWidth: 500px)': {
       width: '400px'
     },
-    '@media (max-width: 500px': {
+    '@media (maxWidth: 500px': {
       '.input-style': {
         width:'100%',
         boxSizing:'border-box'
       }
     }
   }
-
-  shouldComponentUpdate(nextProps, nexState) {
-    console.log('[Person.js] ' + this.props.name + ' shouldComponentUpdate')
-    if (this.props.name !== nextProps.name)
-      return true
-    return false
-  }
-
-  render() {
-    const { name, age, hobby, click, changed } = this.props
-    console.log('[Person.js] ' + this.props.name + ' rendering')
-    return (
-      <div className='Person' style={this.style}>
-        <p onClick={click}>I'm {name}, I'm {age} years old!</p>
-        <p>{hobby ? '& I\'m love ' : null } {hobby}</p>
-        <input type='text' onChange={changed} value={name} style={{width:'90%',boxSizing:'border-box'}} />
-      </div>
-    )
-  }
+  return (
+    <div className='Person' style={style}>
+      <p onClick={click}>I'm {name}, I'm {age} years old!</p>
+      <p>{hobby ? '& I\'m love ' : null } {hobby}</p>
+      <input type='text' onChange={changed} value={name} style={{width:'50%',boxSizing:'border-box'}} />
+    </div>
+  )
 }
 
-export default Radium(Person);
+export default memo(person)
+
 
