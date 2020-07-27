d8144494207bc3ba77544bee91c626891f9994bc
Bear <wokhug99@gmail.com>
2020-07-27 18:09:26 +0700

    255. Setting Up Reducer and Store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
+import Counter from './containers/Counter/Counter';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <h1>Hello, I'm learning React again!</h1>
+      <Counter />
     </div>
   );
 }
