9398db31f309316864866a531678ca977312a6b8
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-07-28 23:24:01 +0700

    260. Dispatching Actions from within the Component

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,14 @@
 const intialState = {
-  counter: 69
+  counter: 2
 }
 
 const reducer = (state = intialState, action) => {
+  if (action.type === 'INC') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    }
+  }
   return state
 }
 
