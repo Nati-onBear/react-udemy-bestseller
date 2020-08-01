9af37600f7ac9a9a998b18c781e5eb4e4d85e90f
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-08-02 00:37:24 +0700

    Basic State

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
 
+import Persons from './containers/Persons';
+
 function App(props) {
   return (
     <div className="App">
@@ -9,7 +11,10 @@ function App(props) {
         <img src={logo} className="App-logo" alt="logo" />
         <p>{props.title}</p>
       </header>
-      
+      <ol>
+        <li>Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
+      </ol>
+      <Persons />
     </div>
   );
 }
